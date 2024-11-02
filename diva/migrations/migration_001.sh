ea39cda9c694b6fcd4f2aab4361b4b0d921d3403
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-02 11:41:07 +0100

    fix migration

--- diva/migrations/migration_001.sh
+++ diva/migrations/migration_001.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-response=$(curl -s -X 'GET' \
-  'http://localhost:30000/api/v2/participant/dkgs' \
-  -H 'accept: application/json' \
-  -H "Authorization: Bearer $DIVA_API_KEY")
-
-old_dkgs=$(echo "$response" | jq -r '. | if .error then 1 else 0 end')
-
-if [ "$old_dkgs" -eq 1 ] 2>/dev/null; then
-  code=$(echo "$response" | jq -r '.error.code')
-
-  if [ "$code" == "PARTICIPANT_DELETING_ALL_DKGS" ]; then
-    curl -s -X 'DELETE' \
+(
+  response=""
+  while [ -z "$response" ]; do
+    response=$(curl -s -X 'GET' \
       'http://localhost:30000/api/v2/participant/dkgs' \
       -H 'accept: application/json' \
-      -H "Authorization: Bearer $DIVA_API_KEY"
+      -H "Authorization: Bearer $DIVA_API_KEY")
+
+    if [ -z "$response" ]; then
+      sleep 5
+    fi
+  done
+
+  old_dkgs=$(echo "$response" | jq -r '. | if .error then 1 else 0 end')
+
+  if [ "$old_dkgs" -eq 1 ]; then
+    code=$(echo "$response" | jq -r '.error.code')
+
+    if [ "$code" == "PARTICIPANT_DELETING_ALL_DKGS" ]; then
+      curl -s -X 'DELETE' \
+        'http://localhost:30000/api/v2/participant/dkgs' \
+        -H 'accept: application/json' \
+        -H "Authorization: Bearer $DIVA_API_KEY"
+    fi
   fi
-fi
+) &
